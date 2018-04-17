b122bc9081eb52524722193d69df94ffbf2506fe
Dario Dematties <dariodematties@yahoo.com.ar>
2018-04-17 12:24:47 -0300

    Important changes incorporated

--- Generate_Samples.py
+++ Generate_Samples.py
@@ -24,7 +24,7 @@
 '''
 Use example:
 
-	Generate_Samples(True, 1)
+	Generate_Samples(True, 1, 10000)
 '''
 
 
@@ -33,26 +33,31 @@ import matplotlib.pyplot as plt
 import Gaussian_Mixture_rnd as gmm
 import scipy.io as sio
 
-def Generate_Samples(plotDistributions, numberOfFiles):
+def Generate_Samples(plotDistributions, numberOfFiles, numberOfSamples):
 
     if (type(plotDistributions) is not bool) or \
        (type(numberOfFiles) is not int) or \
+       (type(numberOfSamples) is not int) or \
+       (numberOfSamples <= 0) or \
        (numberOfFiles <= 0) :
         print("In function Generate_Samples: plotDistributions is not bool or "\
               "numberOfFiles is not int or "\
+              "numberOfSamples is not int or "\
+              "numberOfSamples <= 0 or " \
               "numberOfFiles <= 0")
         raise ValueError('Generate_Samples error')
 
     # Parameters to generate the mixtures of Gaussians
-    n = 10000
-    w = np.array([0.1,0.6,0.05,0.25])
-    mean = np.array([[0,0],[4,4],[4,1],[1,4]])
-    cov = np.array([[[5,0],[0,1]],[[4,0],[0,10]],[[2,1],[1,2]],[[2,-1],[-1,2]]])
+    n = numberOfSamples
+    w = np.array([0.1,0.4,0.1,0.2,0.2])
+    mean = np.array([[0,0],[8,8],[2.5,5],[5,-2],[-3,6]])
+    cov = np.array([[[5,0],[0,1]],[[4,0],[0,10]],[[2,1],[1,2]],[[2,-1],[-1,2]],[[2,-3],[1,2]]])
 
     for i in range(0,numberOfFiles):
         # Generate training data
-        samples, labels = gmm.Gaussian_Mixture_rnd(n, w, mean, cov)
+        samples, labels = gmm.Gaussian_Mixture_rnd(100*n, w, mean, cov)
     
+        print('\n')
         print("Saving training data " + str(i))
     
         data = {'samples': samples,\
@@ -62,7 +67,7 @@ def Generate_Samples(plotDistributions, numberOfFiles):
         sio.savemat('./' + fileName + str(i), {'data': data})
         print("Traininf data " + str(i) + " saved")
     
-        if plotDistributions:
+        if (plotDistributions) and (i < 1) :
             plt.figure()
             for j in range(0,w.size) :
                 indexes = np.where(labels == j)[0]
@@ -75,9 +80,36 @@ def Generate_Samples(plotDistributions, numberOfFiles):
     
     
     
+        # Generate inference data
+        samples, labels = gmm.Gaussian_Mixture_rnd(n, w, mean, cov)
+    
+        print('\n')
+        print("Saving inference data " + str(i))
+    
+        data = {'samples': samples,\
+                'labels': labels}
+    
+        fileName = "Inference_Data"
+        sio.savemat('./' + fileName + str(i), {'data': data})
+        print("Inference data " + str(i) + " saved")
+    
+        if (plotDistributions) and (i < 1) :
+            plt.figure()
+            for j in range(0,w.size) :
+                indexes = np.where(labels == j)[0]
+                sub_samples = samples[indexes,:]
+                plt.scatter(sub_samples[:,0], sub_samples[:,1], s=20, marker="+")
+    
+        plt.title('Inference data samples ' + str(i))
+    
+    
+    
+    
+    
         # Generate testing data
         samples, labels = gmm.Gaussian_Mixture_rnd(n, w, mean, cov)
     
+        print('\n')
         print("Saving testing data " + str(i))
     
         data = {'samples': samples,\
@@ -85,9 +117,9 @@ def Generate_Samples(plotDistributions, numberOfFiles):
     
         fileName = "Testing_Data"
         sio.savemat('./' + fileName + str(i), {'data': data})
-        print("Traininf data " + str(i) + " saved")
+        print("Testing data " + str(i) + " saved")
     
-        if plotDistributions:
+        if (plotDistributions) and (i < 1) :
             plt.figure()
             for j in range(0,w.size) :
                 indexes = np.where(labels == j)[0]
@@ -95,9 +127,4 @@ def Generate_Samples(plotDistributions, numberOfFiles):
                 plt.scatter(sub_samples[:,0], sub_samples[:,1], s=20, marker="o")
     
         plt.title('Testing data samples ' + str(i))
-    
-    
-    if plotDistributions:
-        plt.show()
-
-
+ 
